82a99c542c949ab9921c35c3351a394375cfb041
LCH-1228 <chanho1228@gmail.com>
2025-03-14 14:31:32 +0900

    add:  Lv4 AbstractOperation class .swift

---
+++ Calculator/Lv4.playground/Sources/AbstractOperation.swift
@@ -0,0 +1,9 @@
+//Calculation 프로토콜을 채택하는 추상화 클래스 AbstractOperation 클래스 정의
+public class AbstractOperation: Calculation {
+
+    public init() {
+        
+    }
+    
+    public func calculate(_ firstNumber: Double, _ secondNumber: Double) {}
+}
