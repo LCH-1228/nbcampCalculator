caa5fd32e79ea2524e553017ffbd2db7f8158b53
Chanho Lee <chanho1228@gmail.com>
2025-03-27 11:42:04 +0900

    add: FloatingPoint 타입변환 toDouble extension 추가

---
+++ Calculator/Lv4.playground/Sources/FloatingPointExtension.swift
@@ -0,0 +1,12 @@
+//
+//  FloatinPointExtension.swift
+//
+//
+//  Created by Chanho Lee on 3/27/25.
+//
+
+public extension FloatingPoint {
+    var toDouble: Double {
+        return self as? Double ?? Double(self as! Float)
+    }
+}
